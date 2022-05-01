463aeaa91649affd83cf33739e660d679926f6f1
stevenpham13 <stevenpham13@aol.com>
2022-05-01 08:36:28 -0700

    Update YasiDonorSchedule.js

--- frontend/src/pages/YasiDonorSchedule.js
+++ frontend/src/pages/YasiDonorSchedule.js
@@ -1,6 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import NavBar from './components/Navbar';
+import NavBar from '../components/Navbar';
 function YasiDonorSchedule (props){
     //define constants or functionalities in here
     //perhaps, this is where we ought to divert?
