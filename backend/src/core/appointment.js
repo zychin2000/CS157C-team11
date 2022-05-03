73ab4c6a36cdecae2d0ca983dc319c307b351259
stevenpham13 <stevenpham13@aol.com>
2022-05-02 22:02:51 -0700

    Update appointment.js
    fixed read, update, and delete to query by id an store_name

--- backend/src/core/appointment.js
+++ backend/src/core/appointment.js
@@ -1,7 +1,6 @@
-const { v4: uuidv4 } = require('uuid');
 const db = require("../config/db")
 
-const scheduleAppointment = (store_name,id = uuidv4,approved = false,description, donor,reserved_date) => {
+const scheduleAppointment = (store_name,id,approved = false,description, donor,reserved_date) => {
     const query = "INSERT INTO Donation_Reservation(store_name,id,approved,description,donor,reserved_date) VALUES (?,?,?,?,?,?)"
     return db.execute(query,[store_name,id,approved,description,donor,reserved_date])
 }
@@ -11,11 +10,11 @@ const getAllAppointment = () => {
     return db.execute(query)
 }
 
-const approve = (id) =>{
-    const query = "SELECT approved FROM Donation_Reservation WHERE id = ?"
-    const approved = db.execute(query,id)
-    query = "UPDATE Donation_Reservation SET approved = ? WHERE id = ?"
-    return db.execute(query,[approved])
+const approve = (store_name,id) =>{
+    const query = "SELECT approved FROM Donation_Reservation WHERE store_name = ? AND id = ?"
+    const approved = db.execute(query,store_name,id)
+    query = "UPDATE Donation_Reservation SET approved = ? WHERE store_name=? AND id = ?"
+    return db.execute(query,[approved,store_name,id])
 }
 
 const getByApproved = (approved) => {
@@ -23,7 +22,8 @@ const getByApproved = (approved) => {
     return db.execute(query,[approved])
 }
 
-const deleteAppointment =(id) => {
-    const query = "DELETE FROM Donation_Reservation WHERE id=?"
-    return db.execute(query,[id])
+const deleteAppointment =(store_name,id) => {
+    const query = "DELETE FROM Donation_Reservation WHERE store_name=? AND id=?"
+    return db.execute(query,[store_name,id])
 }
+module.exports = {scheduleAppointment,approve,getAllAppointment,getByApproved,deleteAppointment}
